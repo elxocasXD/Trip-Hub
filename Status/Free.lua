dfd32ef05519e12893fdaece8bc5f2199f870227
elxocasXD <115422799+elxocasXD@users.noreply.github.com>
2023-03-22 13:43:20 -0500

    Update Free.lua

--- Status/Free.lua
+++ Status/Free.lua
@@ -1,4 +1,4 @@
 getgenv().Status_Free = "Work"
 getgenv().Color_Free = Color3.fromRGB(0, 255, 0)
-getgenv().version_Free = "4.0"
+getgenv().version_Free = "4.1"
 getgenv().Trip_Hub_Free_Status = true
