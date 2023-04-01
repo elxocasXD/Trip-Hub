187d2d48e6c637e853e9c9d456565e6143a0a5ad
Space <115422799+elxocasXD@users.noreply.github.com>
2023-03-31 20:06:30 -0500

    Update Premium.lua

--- Status/Premium.lua
+++ Status/Premium.lua
@@ -1,4 +1,4 @@
-getgenv().Status_Premium = "Work"
-getgenv().Color_Premium = Color3.fromRGB(0, 255, 0)
+getgenv().Status_Premium = "Dont Work"
+getgenv().Color_Premium = Color3.fromRGB(255, 0, 0)
 getgenv().version_Premium = "4.2"
-getgenv().Trip_Hub_Premium_Status = true
+getgenv().Trip_Hub_Premium_Status = false
