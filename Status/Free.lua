93fe41816df91e03c4d096d6c3be23d55d4e542b
Space <115422799+elxocasXD@users.noreply.github.com>
2023-04-20 13:58:56 -0500

    Update Free.lua

--- Status/Free.lua
+++ Status/Free.lua
@@ -1,4 +1,4 @@
-getgenv().Status_Free = "Works"
-getgenv().Color_Free = Color3.fromRGB(0, 255, 0)
+getgenv().Status_Free = "Dont Work"
+getgenv().Color_Free = Color3.fromRGB(255, 0, 0)
 getgenv().version_Free = "4.3"
-getgenv().Trip_Hub_Free_Status = true
+getgenv().Trip_Hub_Free_Status = false
