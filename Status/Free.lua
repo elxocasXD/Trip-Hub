5f4951d2673b43b3584a98f929fc821d2ae162fc
Space <115422799+elxocasXD@users.noreply.github.com>
2024-04-12 11:59:28 -0500

    Update Free.lua

--- Status/Free.lua
+++ Status/Free.lua
@@ -1,3 +1,2 @@
-getgenv().version_Free = "5.8"
 getgenv().Trip_Hub_Free_Status = true
-getgenv().Trip_Hub_Free_Status_Text = "We are updating the script, please be patient"
+getgenv().Trip_Hub_Free_Status_Text = "We are updating Trip Hub, please be patient"
